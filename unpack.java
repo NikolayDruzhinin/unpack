c37567c93c8010390d7d056019b4162902980aea
NikolayDruzhinin <66308649+NikolayDruzhinin@users.noreply.github.com>
2021-03-18 16:37:09 +0300

    Add files via upload

---
+++ unpack.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+public class MyClass {
+
+    public static void main(String[] args) 
+    {
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Input string to unpack");
+        String s = keyboard.next();
+		
+        String res = "";
+        int counter = 0;
+        Vector<Integer> mul = new Vector<>();
+
+		for (int i = 0; i < s.length(); ++i)
+		{
+			char sym = s.charAt(i);
+			if ((sym >= '0') && (sym <= '9'))
+			{
+				counter = counter * 10 + (sym - '0');
+				if ((s.charAt(i+1) >= '0') && (s.charAt(i+1) <= '9')) {}
+				else
+				{
+					mul.add(counter);
+					counter = 0;
+				}
+			}
+			else if (sym != '[' && sym != ']')
+			{
+			    res += sym;
+			}
+			else if (sym == ']')
+			{
+				res += res.repeat(mul.get(mul.size()-1)-1);
+				mul.remove(mul.size()-1);
+			}
+		}
+		System.out.println(res);
+	}
+}
